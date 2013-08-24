01ef945c5405c7f894e0f0d94f953c92e44c4a28
johnwilso02 <johnwilso02@gmail.com>
2013-08-24 14:11:30 -0700

    Update hisam.js

--- hisam.js
+++ hisam.js
@@ -1,5 +1,12 @@
-#!/usr/bin/env node
+var express = require('express');
+var app = express();
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Good afternoon, Sam');
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
